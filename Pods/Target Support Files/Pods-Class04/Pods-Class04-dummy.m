826d199a4b8f7b23b74340a46840b869b543d9e2
123 <>
2019-07-11 15:57:57 +0800

    0711

---
+++ Pods/Target Support Files/Pods-Class04/Pods-Class04-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Class04 : NSObject
+@end
+@implementation PodsDummy_Pods_Class04
+@end
---
+++ Pods/Target Support Files/Pods-Class04Tests/Pods-Class04Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Class04Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Class04Tests
+@end
